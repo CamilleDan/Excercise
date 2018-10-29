4a2f54eecdf8f595fd412adcfe4e96e172f574fa
YangDan <942652559@qq.com>
2018-10-29 21:55:25 +0800

    update

--- python_basic.py
+++ python_basic.py
@@ -100,10 +100,39 @@ for fruit in fruits:  # 第二个实例
 
 #for循环和条件语句结合
 for num in range(10,20):  # 迭代 10 到 20 之间的数字
-   for i in range(2,num): # 根据因子迭代
+   for i in range(2,num): # 根据因子迭代s
       if num%i == 0:      # 确定第一个因子
          j=num/i          # 计算第二个因子
          print '%d 等于 %d * %d' % (num,i,j)
          break            # 跳出当前循环
    else:                  # 循环的 else 部分
       print num, '是一个质数'
+
+#break 语句跳出整个循环
+for word in  'djaiofdnvuaihusi':
+    if word=='h':
+        break
+    print 'the word is %s'%word
+
+var = 10
+while var > 0:
+    print var
+    var -= 1
+    if var <= 5:
+        break
+
+#continue跳出本次循环
+for word in  'djaiofdnvuaihusi':
+    if word=='h':
+        continue
+    print 'the word is %s'%word
+
+var = 10
+while var > 0:
+    var -= 1
+    if var == 5:
+        continue
+    print var
+
+
+
