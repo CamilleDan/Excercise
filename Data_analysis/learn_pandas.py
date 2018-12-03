73ffcdebe6b9f8b851b721b354b9c7ff6815ccee
YangDan <942652559@qq.com>
2018-12-03 17:21:22 +0800

    update

--- Data_analysis/learn_pandas.py
+++ Data_analysis/learn_pandas.py
@@ -81,3 +81,49 @@ print obj
 df4=pd.DataFrame(np.arange(16).reshape(4,4),index=['Ohio','Colorado','Utah','New York'],columns=['one','two','three','four'])
 df4.drop('Ohio')
 df4.drop(['one','two'],axis=1)
+
+arr=np.arange(12.).reshape(3,4)
+print arr-arr[0]#
+
+frame=pd.DataFrame(np.arange(12.).reshape(4,3),columns=list('abc'))
+series=frame.loc[0,['a','b','c']]
+print series
+print frame-series
+
+frame1=pd.DataFrame(np.random.randn(4,4),index=['Ohio','Colorado','Utah','New York'],columns=['one','two','three','four'])
+print frame1
+print np.abs(frame1)
+
+f=lambda x:x.max()-x.min()
+print frame1.apply(f,axis=0)
+print frame1.apply(f,axis=1)
+print frame1
+print frame1.sum(axis=1)#求和
+print frame1.mean(axis=0)#求均值
+
+print frame1.sort_index(axis=1)#根据索引的大小排序
+print frame1.sort_index(axis=0,ascending=False)#根据索引的大小排序,降序
+
+series1=pd.Series([4.,9.2,np.nan,7,np.nan,3])
+print series1.sort_values(ascending=False)#按照数值排序，逆序，所有的nan会被放到最后
+
+print frame1.sort_values(by=['one','two'])#按照某一列或几列的值排序
+
+series2=pd.Series([2,4,2,6,7,1,0,1])
+series2.rank(method='first')#按照出现的顺序排序,可以降序
+series2.rank()#普通排序，两个相同的值会并列同级
+frame1.rank(method='first',axis=0)#匹配行
+frame1.rank(method='first',axis=1)#匹配列
+frame1.rank(method='average')#匹配列
+
+frame2=pd.DataFrame(np.arange(16).reshape(4,4),index=['a','a','b','b'],columns=list('1234'))
+print frame2.loc['a','1']#当index重复索引行是会显示所有相同index的行
+print frame2.index#可检查是否有重复index
+
+frame3=pd.DataFrame([[2,np.nan],[3,4],[2.5,1]])
+frame3.sum(axis=1,skipna=True)
+frame3.idxmax(axis=1)#按列执行，找最大值的索引
+frame3.cumsum(axis=0)#按行执行，累计求和
+frame3.cumprod(axis=0)#按行执行，累计求积
+frame3.cummax(axis=0)#按行执行，累计找最大值
+frame1.describe()#五数汇总
